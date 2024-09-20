029d463af855876ace9ad3375d3296a2957d46e2
SumitKumar-17 <sumitkumar@kgpian.iitkgp.ac.in>
2024-09-20 22:36:18 +0530

    feat:using #define for multiple SQl Tokens

--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
 // #include "binder/binder.h"
 #include <iostream>
 
-int fake();
-
-int yylex();
-
-// int main(int argc, char *argv[]) {
-//   std::cout << "hello world" << std::endl;
-//   hello();
-//   return id(42) + fake();
-// }
-
+int yyparse();
 
 int main(int argc, char *argv[]) {
-  return yylex();
+  return yyparse();
 }
