3f4ae86842cb78232b7ea2cee6c6a1958a39d0ba
SumitKumar-17 <sumitkumar@kgpian.iitkgp.ac.in>
2024-09-21 11:23:36 +0530

    fix:name

--- sql/delete_stmt.cpp
+++ sql/delete_stmt.cpp
@@ -2,7 +2,7 @@
 
 namespace skDB {
     DeleteStmt::~DeleteStmt() = default;
-    DeleteStmt::DeleteStmt(TableName table_name_, Exp *whereExp_) : SQLStmt(xSQL_DELETE), table_name(table_name_),
+    DeleteStmt::DeleteStmt(TableName table_name_, Exp *whereExp_) : SQLStmt(skDB_SQL_DELETE), table_name(table_name_),
                                                                     whereExp(whereExp_) {
     }
 }
